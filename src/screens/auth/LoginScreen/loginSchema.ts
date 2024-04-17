d024351730ffd5b7b5679ac452f57c5ec7299430
willperes <thewidocs@gmail.com>
2024-04-17 18:11:06 -0300

    fix: change login schema password validation

--- src/screens/auth/LoginScreen/loginSchema.ts
+++ src/screens/auth/LoginScreen/loginSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
     email: z.string().email("E-mail inválido"),
-    password: z.string().min(8, "Senha deve ter no mínimo 8 caracteres")
+    password: z.string().min(1, "Senha obrigatória")
 })
 
 export type LoginSchema = z.infer<typeof loginSchema>;
