8a9147e8cdd2d19f273b65a5f03e345399c83365
willperes <thewidocs@gmail.com>
2024-04-22 16:33:05 -0300

    feature: fetching post data from the backend

---
+++ src/api/apiTypes.ts
@@ -0,0 +1,19 @@
+export interface MetaDataPageAPI {
+  total: number;
+  per_page: number;
+  current_page: number;
+  last_page: number;
+  first_page: number;
+  first_page_url: string;
+  last_page_url: string;
+  next_page_url: string | null;
+  previous_page_url: string | null;
+}
+/**
+ * @description Interface que define o formato de uma página de dados na API.
+ * @template Data Tipo de dado que será retornado na página.
+ */
+export interface PageAPI<Data> {
+  meta: MetaDataPageAPI;
+  data: Data[];
+}
--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,8 +1,21 @@
-import { postListMock } from "./postListMock";
-import { Post } from "./types";
+import { PageAPI } from "@api";
 
-async function getList(): Promise<Post[]> {
-  return new Promise(resolve => setTimeout(() => resolve(postListMock), 250));
+import { PostAPI } from "./postTypes";
+
+type PostResponseAPI = PageAPI<PostAPI>;
+
+async function getList(): Promise<PostResponseAPI> {
+  const response = await fetch("http://localhost:3333/user/post", {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer Ng.4w4pZ8OFK2Q7f5PzzCmqQdlcrmX3u__HBwB76Ho_rSSgfw8Zf3gh2-XtYYW6",
+    },
+  });
+
+  const data: PostResponseAPI = await response.json();
+
+  return data;
 }
 
 export const postApi = {
